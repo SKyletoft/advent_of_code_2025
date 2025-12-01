207add4aa72b225bf8255d84fae3d3d48f3665e4
Samuel Kyletoft <samuel@kyletoft.se>
2025-12-01 15:27:13 +0100

    Day1: Both

---
+++ dec01/Main.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE GHC2024 #-}
+
+import Data.FileEmbed (embedFile)
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as C8
+
+input :: String
+input = C8.unpack $(embedFile "input.txt")
+
+parse :: String -> Int
+parse = \case
+  ('R':xs) -> read xs
+  ('L':xs) -> -(read xs)
+  _ -> error "Invalid"
+
+wrapAdd x y = (x + y) `mod` 100
+
+part1 :: Int
+part1
+  = length
+  . filter (== 0)
+  . scanl wrapAdd 50
+  . map parse
+  . lines
+  $ input
+
+flatten x
+  | x < 0 = replicate (abs x) (-1)
+  | otherwise = replicate x 1
+
+part2 :: Int
+  = length
+  . filter (== 0)
+  . scanl wrapAdd 50
+  . concatMap flatten
+  . map parse
+  . lines
+  $ input
+
+main :: IO ()
+main = print part2
