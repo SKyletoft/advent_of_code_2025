88bdc9d24c518347552cd994ef6f6757d8a64a86
Samuel Kyletoft <samuel@kyletoft.se>
2025-12-08 14:58:40 +0100

    Day8: Slight optimisation

--- dec08/Main.hs
+++ dec08/Main.hs
@@ -4,8 +4,12 @@
 
 import Data.ByteString.Char8 (unpack)
 import Data.FileEmbed (embedFile)
-import Data.List (nub, sort, sortOn)
+import Data.List (nub, sort, sortBy, sortOn)
 import Data.List.Split (splitOn)
+import Data.Ord (Down (..), comparing)
+import Data.Set (Set)
+
+import Data.Set qualified as S
 
 input :: String
 input = unpack $(embedFile "input.txt")
@@ -15,57 +19,51 @@ type Box = (Int, Int, Int)
 pos :: [Int] -> Box
 pos [x, y, z] = (x, y, z)
 
-dist :: Box -> Box -> Int
-dist (a, b, c) (x, y, z) =
+dist :: (Box, Box) -> Int
+dist ((a, b, c), (x, y, z)) =
   floor . sqrt
     $ fromIntegral (a - x) ** 2
       + fromIntegral (b - y) ** 2
       + fromIntegral (c - z) ** 2
 
+connections :: [Box] -> [(Box, Box)]
 connections boxes =
-  nub . sortOn (uncurry dist)
-    $ [(min x y, max x y) | x <- boxes, y <- boxes, x /= y]
+  sortOn dist
+    $ [(min x y, max x y) | (idx, x) <- zip [0 ..] boxes, y <- drop idx boxes, x /= y]
 
+with :: (a -> Bool) -> [a] -> a
 with f = head . filter f
 
-unify :: Eq a => [[a]] -> (a, a) -> [[a]]
-unify sets (x, y) =
-  let withX = with (x `elem`) sets
-      withY = with (y `elem`) sets
-      without = filter (\c -> not (x `elem` c || y `elem` c)) sets
-   in if withX == withY
-        then sets
-        else (withX ++ withY) : without
-
-g :: Eq a => [[a]] -> a -> Int
-g (c : cs) x | x `elem` c = 0
-g (c : cs) x = 1 + g cs x
-g [] _ = -1
-
-part1 = do
-  let boxes = map (pos . map read . splitOn ",") . lines $ input
-  let sortedConnections = connections boxes
-  print
-    . product
-    . take 3
-    . reverse
-    . sort
-    . map length
-    . (!! 1000)
-    . scanl unify (map (: []) boxes)
-    $ sortedConnections
+unify :: (Eq a, Ord a) => [Set a] -> (a, a) -> [Set a]
+unify sets (x, y)
+  | withX == withY = sets
+  | otherwise = S.union withX withY : without
+ where
+  withX = with (x `S.member`) sets
+  withY = with (y `S.member`) sets
+  without = filter (\c -> not (x `S.member` c || y `S.member` c)) sets
 
 main = do
   let boxes = map (pos . map read . splitOn ",") . lines $ input
   let sortedConnections = connections boxes
+  let stream =
+        scanl unify (map S.singleton boxes)
+          sortedConnections
   let lastConnection =
-        head
+        S.findMin
           . (!! 1)
           . last
-          . takeWhile (\c -> length c > 1)
-          . scanl unify (map (: []) boxes)
+          . takeWhile (\case [_] -> False ; _ -> True)
+          $ stream
+  let part1 =
+        product
+          . take 3
+          . sortBy (comparing Down)
+          . map length
+          . (!! 1000)
+          $ stream
+  let part2 =
+        (\((x, _, _), (a, _, _)) -> x * a)
+          . with (\(x, y) -> x == lastConnection || y == lastConnection)
           $ sortedConnections
-  print
-    . (\((x, _, _), (a, _, _)) -> x * a)
-    . with (\(x, y) -> x == lastConnection || y == lastConnection)
-    $ sortedConnections
+  print (part1, part2)
