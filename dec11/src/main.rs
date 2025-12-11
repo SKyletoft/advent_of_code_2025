d1c8d277062e15250864cbe02ebf3533fa932dc4
Samuel Kyletoft <samuel@kyletoft.se>
2025-12-11 10:21:02 +0100

    Day11: Try inline vecs

--- dec11/src/main.rs
+++ dec11/src/main.rs
@@ -1,5 +1,7 @@
 use std::{collections::HashMap, io::Read};
 
+type SmallVec<T> = smallvec::SmallVec<[T; 20]>;
+
 fn main() {
 	let input = {
 		let mut s = String::new();
@@ -11,24 +13,13 @@ fn main() {
 	println!("part2: {}", part2(&parsed));
 }
 
-fn part1(input: &HashMap<u16, Vec<u16>>) -> u64 {
-	fn find_path(map: &HashMap<u16, Vec<u16>>, at: u16) -> u64 {
-		if at == OUT {
-			return 1;
-		}
-		let nexts = &map[&at];
-		nexts.iter().map(|&next| find_path(map, next)).sum()
-	}
-	find_path(input, YOU)
-}
-
 const OUT: u16 = u16::MAX & 0b0011_1111_1111_1111;
 const FFT: u16 = (u16::MAX - 1) & 0b0011_1111_1111_1111;
 const DAC: u16 = (u16::MAX - 2) & 0b0011_1111_1111_1111;
 const SVR: u16 = (u16::MAX - 3) & 0b0011_1111_1111_1111;
 const YOU: u16 = (u16::MAX - 4) & 0b0011_1111_1111_1111;
 
-fn parse(input: &str) -> HashMap<u16, Vec<u16>> {
+fn parse(input: &str) -> HashMap<u16, SmallVec<u16>> {
 	fn specials(val: &str) -> Option<u16> {
 		match val {
 			"out" => Some(OUT),
@@ -53,20 +44,31 @@ fn parse(input: &str) -> HashMap<u16, Vec<u16>> {
 			let key = words.next().unwrap();
 			let vals = words
 				.map(|val| specials(val).unwrap_or_else(|| ids[val]))
-				.collect::<Vec<u16>>();
+				.collect::<SmallVec<u16>>();
 			(specials(key).unwrap_or_else(|| ids[key]), vals)
 		})
-		.collect::<HashMap<u16, Vec<u16>>>()
+		.collect::<HashMap<u16, SmallVec<u16>>>()
 }
 
-fn part2(input: &HashMap<u16, Vec<u16>>) -> u64 {
+fn part1(input: &HashMap<u16, SmallVec<u16>>) -> u64 {
+	fn find_path(map: &HashMap<u16, SmallVec<u16>>, at: u16) -> u64 {
+		if at == OUT {
+			return 1;
+		}
+		let nexts = &map[&at];
+		nexts.iter().map(|&next| find_path(map, next)).sum()
+	}
+	find_path(input, YOU)
+}
+
+fn part2(input: &HashMap<u16, SmallVec<u16>>) -> u64 {
 	fn to_key(val: u16, a: bool, b: bool) -> u16 {
 		debug_assert!(val & 0b1100_0000_0000_0000 == 0, "{val}");
 		val | ((a as u16) << 15) | ((b as u16) << 14)
 	}
-	fn find_path_2<'a>(
+	fn find_path_2(
 		cache: &mut HashMap<u16, u64>,
-		map: &HashMap<u16, Vec<u16>>,
+		map: &HashMap<u16, SmallVec<u16>>,
 		at: u16,
 		mut seen_dac: bool,
 		mut seen_fft: bool,
